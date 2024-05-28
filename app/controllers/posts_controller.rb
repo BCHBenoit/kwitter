6bc3be3a8880503565029833487b24dbcdfbce17
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-05-28 01:01:23 +0000

    guardado6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+  def index
+    @post_table = Post.all
+    
+    @users_table = User.all
+    
+  end
+
+  def create
+    @post_table = Post.all
+    @post_new_row = Post.new
+    @post_new_row["body"] = params["body"]
+    @post_new_row["user_id"] = session["user_id"]
+    @post_new_row.save
+    redirect_to "/posts"
+  end
 end
